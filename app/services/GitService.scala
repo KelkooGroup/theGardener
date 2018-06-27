3036d707c2a8da7f0ba91a6e555903c33e332f78
Assane Bodiang <assane.bodiang@kelkoogroup.com>
2018-06-27 16:02:50 +0200

    checkout a branch

--- app/services/GitService.scala
+++ app/services/GitService.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import javax.inject.Inject
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.internal.storage.file.FileRepository
 import play.api.Logger
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -13,26 +14,26 @@ class GitService @Inject()(implicit ec: ExecutionContext) {
 
   def clone(url: String, localDirectory: String): Future[Unit] = {
     Future {
-      val featureFile = new File(localDirectory)
-      val log = Logger
-
-
-      log.info("Cloning " + url)
-      log.info("to " + featureFile)
-
-      Git.cloneRepository().setURI(url).setDirectory(featureFile).call()
+      Git.cloneRepository().setURI(url).setDirectory(new File(localDirectory)).call()
+      Logger.info(s"Cloning $url to $localDirectory")
     }
-
-
   }
 
 
-  def checkout(branch: String, localRepository: String): Future[Unit] = {
-    ???
+  def checkout(branch: String, localRepository: String): Future[Unit] = Future {
+    Future {
+      val git = new Git(new FileRepository(localRepository))
+      git.checkout().setName(branch).setCreateBranch(true).addPath("target/data/gitService/local/test").call()
+      Logger.info(s"checkout a $branch to $localRepository")
+    }
   }
 
   def pull(localRepository: String): Future[Unit] = {
-    ???
+    Future {
+      val git = new Git(new FileRepository(localRepository))
+      git.pull().call()
+      Logger.info(s"pull to $localRepository")
+    }
   }
 
 
--- test/services/GitServiceTest.scala
+++ test/services/GitServiceTest.scala
@@ -3,8 +3,8 @@ package services
 
 import java.io.File
 
-import org.apache.commons.io.FileUtils
-import org.eclipse.jgit.api._
+import org.apache.commons.io.FileUtils._
+import org.eclipse.jgit.api.Git
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 
@@ -20,20 +20,26 @@ class GitServiceTest extends WordSpec with MustMatchers with BeforeAndAfter with
   val localFile = new File(localRepositoryDirectory, "test.txt")
 
 
-  before {
-    FileUtils.forceMkdirParent(remoteFile)
-    FileUtils.write(remoteFile, "test", "UTF-8")
+  var git: Git = _
 
-    val git = Git.init().setDirectory(remoteRepositoryDirectory).call()
+  before {
+    git = Git.init().setDirectory(remoteRepositoryDirectory).call()
+    addFileToRemote(git, remoteFile, "test commit")
+  }
+
+  private def addFileToRemote(git: Git, file: File, message: String) = {
+    forceMkdirParent(file)
+    write(file, "test", "UTF-8")
+
 
     git.add().addFilepattern(".").call()
 
-    git.commit().setMessage("test commit").call()
+    git.commit().setMessage(message).call()
   }
 
   after {
-    FileUtils.deleteDirectory(remoteRepositoryDirectory)
-    FileUtils.deleteDirectory(new File(localRepositoryDirectory))
+    deleteDirectory(remoteRepositoryDirectory)
+    deleteDirectory(new File(localRepositoryDirectory))
   }
 
   "GitService" should {
@@ -41,16 +47,40 @@ class GitServiceTest extends WordSpec with MustMatchers with BeforeAndAfter with
       val future = new GitService().clone(remoteRepositoryDirectory.toURI.toString, localRepositoryDirectory)
 
       whenReady(future, timeout(30.seconds)) { _ =>
-        FileUtils.readFileToString(localFile, "UTF-8") mustBe FileUtils.readFileToString(remoteFile, "UTF-8")
+        readFileToString(localFile, "UTF-8") mustBe readFileToString(remoteFile, "UTF-8")
       }
     }
 
-    "checkout a branch" ignore {
-      //val future = new GitService().checkout()
+    "checkout a branch" in {
+      val future = new GitService().checkout("test", localRepositoryDirectory)
+      val remoteFile1 = new File(remoteRepositoryDirectory, "test1.txt")
+      val localFile1 = new File(localRepositoryDirectory, "test1.txt")
+
+      git.branchCreate().setName("test").call()
+      git.checkout().setName("test").call()
+
+      addFileToRemote(git, remoteFile1, " created test to remoteRepository")
+      addFileToRemote(git, localFile1, " created test to localRepository")
+
+      whenReady(future, timeout(30.seconds)) { _ =>
+        readFileToString(remoteFile1, "UTF-8") mustBe readFileToString(localFile1, "UTF-8")
+      }
     }
 
     "pull the updates from remote " ignore {
-      ???
+      val future = new GitService().pull(localRepositoryDirectory)
+      val remoteFile2 = new File(remoteRepositoryDirectory, "test2.txt")
+      val localFile2 = new File(localRepositoryDirectory, "test2.txt")
+
+      git.fetch().setRemote(remoteFile2.getPath).call()
+      git.pull()
+
+      addFileToRemote(git, remoteFile2, "created test2")
+      addFileToRemote(git, localFile2, "created test2")
+
+      whenReady(future, timeout(30.seconds)) { _ =>
+        readFileToString(remoteFile2, "UTF-8") mustBe readFileToString(localFile2, "UTF-8")
+      }
     }
   }
 }
