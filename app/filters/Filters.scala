902b31450311f457c0b16e97025b2a00c679ab4e
Florian Fauvarque <florian.fauvarque@kelkoo.com>
2018-11-20 15:56:52 +0100

    Add Gzip filter

---
+++ app/filters/Filters.scala
@@ -0,0 +1,39 @@
+package filters
+
+import akka.stream.Materializer
+import javax.inject.Inject
+import play.api.http.HttpFilters
+import play.api.mvc.{EssentialFilter, RequestHeader, Result}
+import play.api.{Environment, Mode}
+import play.filters.cors.CORSFilter
+import play.filters.gzip.GzipFilter
+
+class Filters @Inject()(environment: Environment, corsFilter: CORSFilter)(implicit mat: Materializer) extends HttpFilters {
+
+  /**
+    * Returns true for following cases:
+    * - Response is a Javascript file (Angular app)
+    * - Request is made on '/api/generateDocumentation'
+    *
+    * @return
+    */
+  private def shouldGzip = (requestHeader: RequestHeader, response: Result) => {
+    val responseIsJavascript = response.body.contentType.exists(_.startsWith("application/javascript"))
+    val requestPathShouldBeGzipped = requestHeader.path match {
+      case "/api/generateDocumentation" => true
+      case _ => false
+    }
+    responseIsJavascript || requestPathShouldBeGzipped
+  }
+
+  private val gzipFilter = new GzipFilter(shouldGzip = shouldGzip)
+
+  override val filters: Seq[EssentialFilter] = environment.mode match {
+    case Mode.Dev =>
+      // CORSFilter only for DEV mode: allow Angular app to call API on different port
+      Seq(gzipFilter, corsFilter)
+    case _ =>
+      Seq(gzipFilter)
+  }
+
+}
