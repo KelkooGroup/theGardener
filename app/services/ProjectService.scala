6d97778a6884c24f5e016dfc78fa282e26e6af89
GeReinhart <gerald.reinhart@gmail.com>
2019-10-23 13:22:46 +0200

    prevent from having 2 synchro at the same time

--- app/services/ProjectService.scala
+++ app/services/ProjectService.scala
@@ -209,13 +209,38 @@ class ProjectService @Inject()(projectRepository: ProjectRepository, gitService:
     } else Future.successful(())
   }
 
+  val lockFile = s"${projectsRootDirectory}globalSynchroOnGoing".fixPathSeparator
+
+  def isGlobalSynchroOnGoing(): Boolean = {
+    new File(lockFile).exists()
+  }
+
+  def canStartGlobalSynchro(): Boolean = {
+    if (isGlobalSynchroOnGoing) {
+      false
+    } else {
+      new File(lockFile).createNewFile()
+    }
+  }
+
+  def finishGlobalSynchro(): Boolean = new File(lockFile).delete()
+
   def synchronizeAll(): Future[Unit] = {
-    logger.info("Start synchronizing projects")
 
-    val projects = projectRepository.findAll()
+    if (canStartGlobalSynchro) {
+      logger.info("Start synchronizing projects")
 
-    FutureExt.sequentially(projects)(synchronize).flatMap(_ => Future.fromTry(menuService.refreshCache())).map { _ =>
-      logger.info(s"Synchronization of ${projects.size} projects is finished")
+      val projects = projectRepository.findAll()
+
+      FutureExt.sequentially(projects)(synchronize).flatMap(_ => Future.fromTry(menuService.refreshCache())).map { _ =>
+        logger.info(s"Synchronization of ${projects.size} projects is finished")
+        finishGlobalSynchro
+        ()
+      }
+
+    } else {
+      logger.info("Synchronization already ongoing. Abort")
+      Future.successful(())
     }
   }
 
