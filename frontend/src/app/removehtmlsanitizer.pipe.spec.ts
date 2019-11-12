622d2c70c35ea9dfd749e02db337947ba6d7ee92
Martial Le Cun <martial.lecun@free.fr>
2019-11-12 10:33:52 +0100

    add test for removeHtmlSanitizer

--- frontend/src/app/removehtmlsanitizer.pipe.spec.ts
+++ frontend/src/app/removehtmlsanitizer.pipe.spec.ts
@@ -3,9 +3,21 @@ import {DomSanitizer} from '@angular/platform-browser';
 import {TestBed} from '@angular/core/testing';
 
 describe('RemovehtmlsanitizerPipe', () => {
-  it('create an instance', () => {
-    const sanitizer: DomSanitizer = TestBed.get(DomSanitizer);
-    const pipe = new RemoveHtmlSanitizerPipe(sanitizer);
-    expect(pipe).toBeTruthy();
+  let sanitizer: DomSanitizer;
+  let removeSanitizerPipe: RemoveHtmlSanitizerPipe;
+
+  beforeEach(() => {
+    sanitizer = TestBed.get(DomSanitizer);
+    removeSanitizerPipe = new RemoveHtmlSanitizerPipe(sanitizer);
   });
+
+  it('create an instance', () => {
+    expect(removeSanitizerPipe).toBeTruthy();
+  });
+
+  it('Remove sanitizer', () => {
+    expect(sanitizer.sanitize(1,removeSanitizerPipe.transform(htmlTest))).toBe(htmlTest)
+  });
+
+  const htmlTest = '<p>For instance :\n<a onclick="navigateTo(\'app/documentation/navigate/_tools;path=theGardener%3Emaster%3E_guides_/install\')">internal link to installation guide of the project theGardener</a></p>'
 });
