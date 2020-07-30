418773e150e5910b93d246b645c05a9d11ec17d5
GeReinhart <gerald.reinhart@gmail.com>
2020-07-30 17:36:17 +0200

    add documentation

--- app/models/HierarchyNode.scala
+++ app/models/HierarchyNode.scala
@@ -1,6 +1,18 @@
 package models
 
-case class HierarchyNode(id: String, slugName: String, name: String, childrenLabel: String, childLabel: String, directoryPath: Option[String]= None, shortcut: Option[String]= None)
+import com.github.ghik.silencer.silent
+import io.swagger.annotations.ApiModelProperty
+
+@silent("Interpolated")
+@silent("missing interpolator")
+case class HierarchyNode(
+                          @ApiModelProperty(value = "id of the node, define the hierarchy by it's structure, should match the pattern (\\.[0-9]+)*\\.).", example = ".01.02.01.", required = true) id: String,
+                          @ApiModelProperty(value = "name of the node used in the url", example = "tools", required = true) slugName: String,
+                          @ApiModelProperty(value = "label of the node used in the application", example = "tools", required = true) name: String,
+                          @ApiModelProperty(value = "child element type (plural)",example = "project") childrenLabel: String,
+                          @ApiModelProperty(value = "child element type (singular)",example = "project") childLabel: String,
+                          @ApiModelProperty(value = "directory of pages displayed at this level, use the backend representation",example = "publisherData>master>/Public/") directoryPath: Option[String]= None,
+                          @ApiModelProperty(value = "shortcut to another node of the hierarchy, use the id",example = ".02.01.") shortcut: Option[String]= None)
 
 object HierarchyNode {
   val idSeparator = "."
