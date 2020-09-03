4efd65e3dcdb44b2c8c6b0cd69d43b9685e4baec
GeReinhart <gerald.reinhart@gmail.com>
2020-09-03 18:54:08 +0200

    rollback refactoring lucene

--- app/services/SearchService.scala
+++ app/services/SearchService.scala
@@ -1,9 +1,7 @@
 package services
 
-import com.outr.lucene4s.field.Field
-import com.outr.lucene4s.mapper.Searchable
-import com.outr.lucene4s.query.{SearchTerm, Sort}
-import com.outr.lucene4s.{DirectLucene, parse, _}
+import com.outr.lucene4s.query.{Sort, TermSearchTerm}
+import com.outr.lucene4s.{DirectLucene, parse}
 import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
 
@@ -14,12 +12,6 @@ case class SearchResultItem(page: PageIndexDocument, highlights: Seq[Highlighted
 case class PageIndexDocument(id: String, hierarchy: String, path: String, breadcrumb: String, project: String, branch: String, label: String, description: String, pageContent: String)
 
 case class HighlightedFragment(fragment: String, word: String)
-
-case class PageIndexLucene(id: Id[PageIndexLucene], hierarchy: String, path: String, breadcrumb: String, project: String, branch: String, label: String, description: String, pageContent: String)
-
-case class Id[T](value: String) {
-  override def toString: String = value
-}
 
 object HighlightedFragment {
   implicit val format = Json.format[HighlightedFragment]
@@ -38,51 +30,50 @@ object SearchResult {
 }
 
 
-trait SearchablePageIndexLucene extends Searchable[PageIndexLucene] {
-  // We must implement the criteria for updating and deleting
-  override def idSearchTerms(t: PageIndexLucene): List[SearchTerm] = List(exact(id(t.id)))
-
-  implicit def stringifyId[T]: Stringify[Id[T]] = Stringify[Id[T]]((s: String) => Id[T](s))
-
-  // We can create custom / explicit configurations for each field
-  val id: Field[Id[PageIndexLucene]] = lucene.create.stringifiedField[Id[PageIndexLucene]]("id", fullTextSearchable = false)
-
-  def hierarchy: Field[String]
-
-  def path: Field[String]
-
-  def breadcrumb: Field[String]
-
-  def project: Field[String]
-
-  def branch: Field[String]
-
-  def label: Field[String]
-
-  def description: Field[String]
-
-  def pageContent: Field[String]
-
-}
-
-
-object lucene extends DirectLucene(uniqueFields = List("id"), defaultFullTextSearchable = true, appendIfExists = false, autoCommit = true) {
-  val page: SearchablePageIndexLucene = create.searchable[SearchablePageIndexLucene]
-}
-
-
 @Singleton
 class IndexService {
 
+  val luceneSearchIndex = new DirectLucene(uniqueFields = List("id"), defaultFullTextSearchable = true, appendIfExists = false, autoCommit = true)
+
+  private val id = luceneSearchIndex.create.field[String]("id")
+  private val hierarchy = luceneSearchIndex.create.field[String]("hierarchy")
+  private val path = luceneSearchIndex.create.field[String]("path", fullTextSearchable = false)
+  private val breadcrumb = luceneSearchIndex.create.field[String]("breadcrumb")
+  private val project = luceneSearchIndex.create.field[String]("project")
+  private val branch = luceneSearchIndex.create.field[String]("branch")
+  private val label = luceneSearchIndex.create.field[String]("label")
+  private val description = luceneSearchIndex.create.field[String]("description")
+  private val pageContent = luceneSearchIndex.create.field[String]("pageContent", sortable = false)
+
   def insertOrUpdateDocument(document: PageIndexDocument): Unit = {
-    val docLucene = PageIndexLucene(Id[PageIndexLucene](document.id), document.hierarchy, document.path, document.breadcrumb, document.project, document.branch, document.label, document.description, document.pageContent)
-    lucene.page.delete(docLucene)
-    lucene.page.insert(docLucene)
+
+    var i = 0
+    while (exists(document) && i < 10) {
+      luceneSearchIndex.delete(new TermSearchTerm(Some(id), document.id))
+      luceneSearchIndex.commit()
+      i = i + 1
+    }
+
+    luceneSearchIndex.doc().fields(id(document.id),
+      hierarchy(document.hierarchy),
+      path(document.path),
+      breadcrumb(document.breadcrumb),
+      project(document.project),
+      branch(document.branch),
+      label(document.label),
+      description(document.description),
+      pageContent(document.pageContent)
+    ).index()
+    luceneSearchIndex.commit()
     ()
   }
 
+  private def exists(document: PageIndexDocument) = {
+    luceneSearchIndex.query().filter(new TermSearchTerm(Some(id), document.id)).search().results.length > 0
+  }
+
   def query(keywords: String): SearchResult = {
-    val results = lucene.page.query().sort(Sort.Score).filter(parse("id:" + keywords + "*^20 " + " | "
+    val results = luceneSearchIndex.query().sort(Sort.Score).filter(parse("id:" + keywords + "*^20 " + " | "
       + "breadcrumb:" + keywords + "*^50 " + " | "
       + "project:" + keywords + "*^30 " + " | "
       + "branch:" + keywords + "*^30" + " | "
@@ -92,24 +83,16 @@ class IndexService {
     )).highlight().limit(50).search()
     SearchResult(results.results.map {
       result =>
-        val highlighting = result.highlighting(lucene.page.breadcrumb).map(frg => HighlightedFragment(frg.fragment, frg.word)) +:
-          result.highlighting(lucene.page.label).map(frg => HighlightedFragment(frg.fragment, frg.word)) +:
-          result.highlighting(lucene.page.description).map(frg => HighlightedFragment(frg.fragment, frg.word)) +:
-          result.highlighting(lucene.page.pageContent).map(frg => HighlightedFragment(frg.fragment, frg.word)) +: Nil
-        SearchResultItem(PageIndexDocument(result(lucene.page.id).value,
-          result(lucene.page.hierarchy),
-          result(lucene.page.path),
-          result(lucene.page.breadcrumb),
-          result(lucene.page.project),
-          result(lucene.page.branch),
-          result(lucene.page.label),
-          result(lucene.page.description),
-          result(lucene.page.pageContent)), highlighting.flatten)
+        val highlighting = result.highlighting(breadcrumb).map(frg => HighlightedFragment(frg.fragment, frg.word)) +:
+          result.highlighting(label).map(frg => HighlightedFragment(frg.fragment, frg.word)) +:
+          result.highlighting(description).map(frg => HighlightedFragment(frg.fragment, frg.word)) +:
+          result.highlighting(pageContent).map(frg => HighlightedFragment(frg.fragment, frg.word)) +: Nil
+        SearchResultItem(PageIndexDocument(result(id), result(hierarchy), result(path), result(breadcrumb), result(project), result(branch), result(label), result(description), result(pageContent)), highlighting.flatten)
     })
   }
 
   def reset(): Unit = {
-    lucene.deleteAll()
+    luceneSearchIndex.deleteAll()
   }
 
 }
